74a445239f35a7f3daf31c16de066fc072ac8edb
LitvinVK <LitvinVK@yandex.ru>
2022-10-31 14:18:11 +0300

    add class PrintStudents100

--- src/XmlDataReader.py
+++ src/XmlDataReader.py
@@ -3,7 +3,6 @@ from Types import DataType
 from DataReader import DataReader
 
 import xml.etree.cElementTree as xml
-import random
 
 
 class XmlDataReader(DataReader):
@@ -27,23 +26,4 @@ class XmlDataReader(DataReader):
                                                 int(student_child.text)))
 
         return self.students
-        # print(self.students)
 
-        # for student in students:
-        #     self.key = student.attrib.get("class")
-        #     student_children = list(student)
-        #     is100 = True
-
-        #     for student_child in student_children:
-        #         if int(student_child.text) != 100:
-        #             is100 = False
-
-        #     if is100:
-        #         self.students100.append(student.attrib.get("class"))
-
-        # if len(self.students100) == 0:
-        #     print("Ни у одного студента нет 100 баллов по всем дисциплинам")
-        # else:
-        #     print("У студента", self.students100
-        #     [random.randint(0, len(self.students100)-1)],
-        #     "100 баллов по всем дисциплинам")
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import argparse
+from decimal import Clamped
 import sys
 
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
 from XmlDataReader import XmlDataReader
+from PrintStudents100 import PrintStudents100
 
 
 def get_path_from_arguments(args) -> str:
@@ -18,12 +20,19 @@ def get_path_from_arguments(args) -> str:
 def main():
     path = get_path_from_arguments(sys.argv[1:])
 
-    reader = XmlDataReader()
+    if (path[-3:] == 'txt'):
+        reader = TextDataReader()
+    else:
+        reader = XmlDataReader()
+    
     students = reader.read(path)
-    print("Students: ", students)
+    print("Students: ", students, "\n")
 
     rating = CalcRating(students).calc()
-    print("Rating: ", rating)
+    print("Rating: ", rating, "\n")
+
+    students100 = PrintStudents100(students).calc()
+    print(students100)
 
 
 if __name__ == "__main__":
